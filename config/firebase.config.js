02d04ed5f3e6f914b3076de37e62ec83cfc0321c
Sarbobhowma <sarbobhowmasen12345@gmail.com>
2025-06-29 02:08:42 +0530

    first-commit

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,12 +1,11 @@
 const Firebase = require('firebase-admin');
 
-const serviceAccount = require('../drive-f3841-firebase-adminsdk-fbsvc-99f528393b.json');
+// Parse the Firebase credentials from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
-const firebase=Firebase.initializeApp({
-    credential: Firebase.credential.cert(serviceAccount),
-    storageBucket: 'drive-f3841.firebasestorage.app'
-})
+const firebase = Firebase.initializeApp({
+  credential: Firebase.credential.cert(serviceAccount),
+  storageBucket: 'drive-f3841.appspot.com'  // fixed your storage bucket URL too
+});
 
-
-
-module.exports=Firebase;
+module.exports = Firebase;
