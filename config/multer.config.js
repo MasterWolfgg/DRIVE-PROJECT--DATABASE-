760fd534e4c2dba6523b4cbf974634ccf99a96cd
Sarbobhowma <sarbobhowmasen12345@gmail.com>
2025-06-29 11:47:20 +0530

    fourth-commit

--- config/multer.config.js
+++ config/multer.config.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
 const multer = require('multer');
 const firebaseStorage = require('multer-firebase-storage');
 const firebase = require('./firebase.config');
-const serviceAccount= require('../drive-f3841-firebase-adminsdk-fbsvc-99f528393b.json')
+// const serviceAccount= require('../drive-f3841-firebase-adminsdk-fbsvc-99f528393b.json')
+
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 
 const storage = firebaseStorage({
